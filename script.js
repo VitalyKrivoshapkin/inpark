a2d77763ef049a415cc22d05cde0226d3d99773a
VitalyKrivoshapkin <vitaly@mytona.com>
2024-05-30 12:00:05 +1200

    fix 1

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
-const matrixContainer = document.querySelector('.matrix-container');
-const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-`~[]{};\':"\\|,.<>/?';
+const matrixColumns = document.querySelectorAll('.matrix-column');
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+=-`~[]{}\|:;"\'<,>.?/';
 
-function createMatrixColumn() {
-  const column = document.createElement('div');
-  column.classList.add('matrix-column');
-  for (let i = 0; i < 20; i++) {
-    const char = document.createElement('span');
-    char.classList.add('matrix-char');
-    char.textContent = characters.charAt(Math.floor(Math.random() * characters.length));
-    column.appendChild(char);
-  }
-  return column;
+function createMatrixChar(column) {
+  const char = document.createElement('span');
+  char.classList.add('matrix-char');
+  char.textContent = characters.charAt(Math.floor(Math.random() * characters.length));
+  column.appendChild(char);
 }
 
-function animateMatrixChar(char) {
-  char.classList.add('show');
-  setTimeout(() => {
-    char.classList.remove('show');
-    setTimeout(() => {
-      char.textContent = characters.charAt(Math.floor(Math.random() * characters.length));
-      animateMatrixChar(char);
-    }, Math.random() * 1000);
-  }, Math.random() * 1000);
-}
-
-for (let i = 0; i < 50; i++) {
-  const column = createMatrixColumn();
-  matrixContainer.appendChild(column);
-
-  const chars = column.querySelectorAll('.matrix-char');
-  chars.forEach(char => {
-    animateMatrixChar(char);
+function animateMatrix() {
+  matrixColumns.forEach(column => {
+    const charCount = Math.floor(Math.random() * 30) + 10;
+    for (let i = 0; i < charCount; i++) {
+      createMatrixChar(column);
+    }
   });
 }
+
+animateMatrix();
+
+setInterval(() => {
+  matrixColumns.forEach(column => {
+    column.firstChild.remove();
+  });
+  animateMatrix();
+}, 100);
